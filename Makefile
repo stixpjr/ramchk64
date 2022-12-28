df2fdf30b84e1747ef2fb75ebeb293dcb26d8220
Paul Ripke <stix@stix.id.au>
2022-12-28 17:24:36 +1100

    Initial release, 20221220
    - checks all RAM from $0000 thru $feff.
    - full bit patterns to each byte - probably overkill.
    - second pass flips bits of each odd byte.
    - code is ~500 bytes, stack placed at end of code.
    - code is moved around to allow testing all RAM.
    - 512 byte video RAM also moved around.

---
+++ Makefile
@@ -0,0 +1,12 @@
+all:	ramchk64.dsk
+
+clean:
+	-rm -f ramchk64.bin ramchk64.dsk
+
+ramchk64.bin:	ramchk64.asm
+	lwasm -9 -b -o $@ ramchk64.asm
+
+ramchk64.dsk:	ramchk64.bin
+	imgtool create coco_jvc_rsdos $@
+	imgtool put coco_jvc_rsdos $@ ramchk64.bin ramchk64.bin
+
---
+++ ramchk64.asm
@@ -0,0 +1,259 @@
+* ramchk64 20221220
+* stix@stix.id.au
+
+* assumes a 64k machine for now
+
+	org	$7800
+zb	jmp	start
+	fdb	ze-zb
+* disable interrupts
+start	orcc	#$50
+* hide ROM
+	clr	romset
+	jmp	p30
+* copy ourselves to page 0
+loop	ldx	#zb
+	ldy	#0
+copy1	ldd	,x++
+	std	,y++
+	cmpx	#ze
+	bls	copy1
+* jump into copy
+	jmp	p0-zb
+* move video RAM to page1, @$200
+p0	clr	f0set+0
+	clr	f0clr+2
+	*clr	f0clr+4
+	*clr	f0clr+6
+	*clr	f0clr+8
+	*clr	f0clr+10
+	*clr	f0clr+12
+	ldd	#$200
+	std	vidram,pcr
+* set up stack
+	lds	#$1ff
+	lbsr	prtscr
+* check blocks from $0400-$fe00
+	lda	#$04
+loop1	bsr	chk
+	cmpa	#$ff
+	bne	loop1
+	inc	cycles,pcr
+* copy ourselves back to $ORG
+	ldx	#0
+	ldy	#zb
+copy2	ldd	,x++
+	std	,y++
+	cmpy	#ze
+	bls	copy2
+* jump into copy
+	jmp	p30
+* move video RAM to page2, @$400
+p30	clr	f0clr+0
+	clr	f0set+2
+	*clr	f0clr+4
+	*clr	f0clr+6
+	*clr	f0clr+8
+	*clr	f0clr+10
+	*clr	f0clr+12
+	ldd	#$400
+	std	vidram,pcr
+* set up stack
+	lds	#zb+$1ff
+	lbsr	prtscr
+* check blocks from $0000-$0300
+	clra
+loop2	bsr	chk
+	cmpa	#$04
+	bne	loop2
+	lbra	loop
+
+* subroutines
+
+* chk - check a page with all patterns
+* inputs:
+* a: 256 byte block number
+chk	ldy	#$69
+* prints page # from a
+	lbsr	prthex
+* patterns from $01-$ff
+	clrb
+* print pattern # in b
+chk1	ldy	#$89
+	exg	a,b
+	bsr	prthex
+	exg	a,b
+	bsr	chkblk
+	incb
+	bne	chk1
+	inca
+	rts
+
+* chkblk
+* inputs:
+* a: 256 byte block number
+* b: fill pattern
+* clobbers: x,y
+chkblk	pshs	a,b
+* 1st pass, same pattern every byte
+	clrb
+	ldx	#0
+	leax	d,x
+	lda	1,s
+	ldy	#$100
+* fill loop
+chkbl1	sta	,x+
+	leay	-1,y
+	bne	chkbl1
+	ldx	#0
+	lda	,s
+	leax	d,x
+	lda	1,s
+	ldy	#$100
+* verify loop
+chkbl2	cmpa	,x+
+	beq	chkbl3
+	bsr	error
+chkbl3	leay	-1,y
+	bne	chkbl2
+	lda	,s
+* 2nd pass, flip bits every 2nd byte
+	ldx	#0
+	leax	d,x
+	lda	1,s
+	ldy	#$100
+* fill loop
+chkbl4	sta	,x+
+	coma
+	leay	-1,y
+	bne	chkbl4
+	ldx	#0
+	lda	,s
+	leax	d,x
+	lda	1,s
+	ldy	#$100
+* verify loop
+chkbl5	cmpa	,x+
+	beq	chkbl6
+	bsr	error
+chkbl6	coma
+	leay	-1,y
+	bne	chkbl5
+	puls	a,b,pc
+
+* error
+* inc error counter, and update on screen
+error	inc	errors,pcr
+	pshs	a,b,x,y
+	lda	errors,pcr
+	ldy	#$c9
+	bsr	prthex
+	puls	a,b,x,y,pc
+
+* prthex
+* inputs:
+* a: hex byte to print
+* y: screen offset
+prthex	pshs	a,b
+	ldd	vidram,pcr
+	leay	d,y
+	lda	,s
+	lsra
+	lsra
+	lsra
+	lsra
+	bsr	prthe1
+	lda	,s
+	anda	#$0f
+	bsr	prthe1
+	puls	a,b,pc
+prthe1	adda	#$70
+	cmpa	#$79
+	bls	prthe2
+	suba	#$39
+prthe2	sta	,y+
+	rts
+
+* prtscr
+* print text headers, cycle & error count
+* clobbers: a,b,x,y
+prtscr	bsr	cls
+	ldy	#0
+	leax	strttl,pcr
+	bsr	prtstr
+	ldy	#$20
+	leax	straut,pcr
+	bsr	prtstr
+	ldy	#$60
+	leax	strpg,pcr
+	bsr	prtstr
+	ldy	#$80
+	leax	strpat,pcr
+	bsr	prtstr
+	ldy	#$a0
+	leax	strcyc,pcr
+	bsr	prtstr
+	ldy	#$a9
+	lda	cycles,pcr
+	bsr	prthex
+	ldy	#$c0
+	leax	strerr,pcr
+	bsr	prtstr
+	ldy	#$c9
+	lda	errors,pcr
+	bsr	prthex
+	rts
+
+* prtstr - print ascii string
+* inputs:
+* x: null-terminated str ptr
+* y: screen offset
+* clobbers: a,b
+prtstr	ldd	vidram,pcr
+	leay	d,y
+prtst1	lda	,x+
+	beq	prtst3
+	cmpa	#$40
+	bhs	prtst2
+	adda	#$40
+prtst2	sta	,y+
+	bra	prtst1
+prtst3	rts
+
+* cls - clear screen at vidram
+* clobbers a,b,x,y
+cls	ldd	#$6060
+	ldx	vidram,pcr
+	ldy	#$200
+cls1	std	,x++
+	leay	-2,y
+	bne	cls1
+	rts
+
+* vars
+vidram	fdb	0
+cycles	fcb	0
+errors	fcb	0
+
+* strings
+strttl	fcc	"RAMCHK64 20221220"
+	fcb	0
+straut	fcc	"BY PAUL RIPKE STIX@STIX.ID.AU"
+	fcb	0
+strpg	fcc	"PAGE:"
+	fcb	0
+strpat	fcc	"PATTERN:"
+	fcb	0
+strcyc	fcc	"CYCLES:"
+	fcb	0
+strerr	fcc	"ERRORS:"
+	fcb	0
+
+* equates
+romclr	equ	$ffde
+romset	equ	$ffdf
+f0clr	equ	$ffc6
+f0set	equ	$ffc7
+
+ze	equ	*
+	end	zb
